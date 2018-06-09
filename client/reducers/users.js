72757f8c6aeb4b9979d6689818948e754a9517aa
joshua-fogg <joshuafoggnz@gmail.com>
2018-06-09 12:17:46 +1200

    wip(users reducer): add data set for defualt value

--- client/reducers/users.js
+++ client/reducers/users.js
@@ -1,6 +1,33 @@
 import { RECEIVE_USERS } from '../actions'
 
-function users (state = [], action) {
+const defaultUserList = [
+  {
+      name: "Jim",
+      age: 30,
+      gender: "male",
+      _id: "b3Fshn8F976TZCTg"
+  },
+  {
+      name: "Jane",
+      age: 55,
+      gender: "female",
+      _id: "k3nEqkqlKmWZNejC"
+  },
+  {
+      name: "Bob",
+      age: 20,
+      gender: "male",
+      _id: "oqnu2ZnPTebp04bG"
+  },
+  {
+      name: "Sally",
+      age: 24,
+      gender: "female",
+      _id: "tKmv8RC6GlUnYcV3"
+  }
+]
+
+function users (state = defaultUserList, action) {
   switch (action.type) {
     case RECEIVE_USERS:
       return action.users
