51cdf52e92c8c299c9d7afb125e033237bf78fde
joshua-fogg <joshuafoggnz@gmail.com>
2018-06-09 18:16:39 +1200

    maintain(webpack): update scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,44 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true, // default is false
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          // "postcss-loader"
+        ]
+      },
+      {
+        test: /\.jsx?$/,
         loader: 'babel-loader',
-        // test: /\.jsx?$/,
-        exclude: /node_modules/
-      }
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test    : /(\.scss|\.css)$/,
+        include : path.join(__dirname, '../../', 'src'),
+        loaders : [ 'style', 'css', 'sass' ],
+      },
+      {
+        test    : /(\.scss|\.css)$/,
+        include : /(node_modules)\/react-toolbox/,
+        loaders : [
+          require.resolve('style-loader'),
+          require.resolve('css-loader') + '?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          require.resolve('sass-loader') + '?sourceMap',
+        ]
+      },
     ]
   },
   resolve: {
