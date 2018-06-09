2e3207d6caa870e129b1c1e61996c3af15f835f7
joshua-fogg <joshuafoggnz@gmail.com>
2018-06-09 12:16:07 +1200

    fix( LoadUsers ): fix references

--- client/components/LoadUsers.jsx
+++ client/components/LoadUsers.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
+import { fetchUsers } from '../actions'
 
 const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+  <button onClick={() => dispatch(fetchUsers())}>
     Fetch Posts
   </button>
 )
