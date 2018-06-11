1c3a529eb9dae463c1889cb58e493fd542de5f11
joshua-fogg <joshuafoggnz@gmail.com>
2018-06-11 12:13:27 +1200

    fix(server): fix api call and add lint

--- server/users.js
+++ server/users.js
@@ -1,14 +1,15 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
-const db = require('./db')
+const router = express.Router();
+const db = require('./db');
 
-router.use(express.json())
+router.use(express.json());
 
 router.get('/', (req, res) => {
-  const UserList = db.getUsers().then(userList => {
-        res.json(UserList)
-    })
-})
+  const userList = db.getUsers();
+  return userList.then(users => {
+    return res.json(users);
+  });
+});
 
-module.exports = router
+module.exports = router;
